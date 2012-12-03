6fa0d43c3f68e24330ba0708efaf1f3a30ffebc9
DI <DI@DIs-MacBook-Pro.local>
2012-12-03 14:22:48 -0500

    update

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 @app.route("/fsq", methods=['GET','POST'])
 def fsqdemo():
 	if request.method == "GET":
-		return render_template('fsq.html')
+		return render_template('main.html')
 
 	elif request.method == "POST":
 
