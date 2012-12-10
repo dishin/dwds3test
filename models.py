118f4ad3cbb376ba1fc5e454fd199473bbcd0886
DI <DI@DIs-MacBook-Pro.local>
2012-12-10 13:58:40 -0500

    update

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class Image(mongoengine.Document):
 
 	#category = mongoengine.StringField(choices = ( ('ride','Ride'), ('tech','Tech'), ('fashion','Fashion'), ('gear','Gear'), ('place','Place')) )
 
-	#venues = mongoengine.StringField()
+	venues = mongoengine.StringField()
 
 	filename = mongoengine.StringField()
 
